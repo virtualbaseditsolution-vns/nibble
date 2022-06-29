122e96fd625e408a94ca20298cc7293fb9db74b0
Virtual Based IT Solutions <108391973+virtualbaseditsolution-vns@users.noreply.github.com>
2022-06-29 17:56:14 +0530

    New Upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nibble
+nibble is a premium quality ui kit of 45 food delivery app screens based on iOS. It's available for Flutter UI
---
+++ android/app/src/main/kotlin/com/vbits/nibble/nibble/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vbits.nibble.nibble
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
